5bb055b47f4c5191efe470bcb358e6ec1da45876
oxyden27 <lucmarie.lm@gmail.com>
2016-10-13 22:26:03 +0200

    js scroll slow

--- src/HETIC/AppBundle/Resources/source/js/global.js
+++ src/HETIC/AppBundle/Resources/source/js/global.js
@@ -7,3 +7,16 @@ $( document ).ready(function() {
       $('header nav').fadeToggle();
     });
   });
+
+
+$(document).ready(function () {
+            $('.slow').on('click', function () { // Au clic sur un élément
+                var page = $(this).attr('href'); // Page cible
+                var speed = 750; // Durée de l'animation (en ms)
+                $('html, body').animate({
+                    scrollTop: $(page).offset().top
+                }, speed); // Go
+                return false;
+            });
+        });
+
